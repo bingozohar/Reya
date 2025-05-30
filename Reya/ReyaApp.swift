997844cfde68664ca6a473743e06aa82d43ccc9b
bingozohar <romi.peli@pm.me>
2025-05-30 23:29:32 +0200

    Initial Commit

---
+++ Reya/ReyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReyaApp.swift
+//  Reya
+//
+//  Created by Romaryc Pelissie on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
